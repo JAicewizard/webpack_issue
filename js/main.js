71fadd13aaceb58c1b0fbec811da0d4449241ddd
jaap aarts <jaap.aarts1@gmail.com>
2020-04-20 15:40:21 +0200

    setup for webpack issue

---
+++ js/main.js
@@ -0,0 +1,15 @@
+ import _ from 'lodash';
+
+  function component() {
+    const element = document.createElement('div');
+
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    return element;
+  }
+
+  document.body.appendChild(component());
+
+export function hi(){
+	console.log("hello world!!!");
+}
